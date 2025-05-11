4e21a98cfc5487f0bba4ffd04ab472f7f857e0ca
Szabó Ádám <112242221+sz-adam@users.noreply.github.com>
2025-05-11 20:21:33 +0200

    add habit model

---
+++ lib/model/habit_model.dart
@@ -0,0 +1,19 @@
+import 'dart:ui';
+
+class Habit {
+  String title;
+  String? description;
+  //Color color;
+  List<int>? selectedDays;
+  int? durationMinutes;
+  DateTime startDate;
+
+  Habit({
+    required this.title,
+    this.description,
+    // required this.color,
+    this.selectedDays,
+    this.durationMinutes,
+    required this.startDate,
+  });
+}
