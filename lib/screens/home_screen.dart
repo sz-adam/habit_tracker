a312b1fa1a4d365a798a0f9eee5a4598615261cc
Szabó Ádám <112242221+sz-adam@users.noreply.github.com>
2025-05-14 12:42:55 +0200

    only the habits of the given day are displayed

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:habit_tracker/widget/Calendar.dart';
 import 'package:habit_tracker/widget/habit_card.dart';
 import '../riverpod/habit_provider.dart';
 import '../riverpod/name_provider.dart';
+import '../riverpod/date_provider.dart';
 
 class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
@@ -17,8 +18,24 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
   Widget build(BuildContext context) {
     final userName = ref.watch(userNameProvider);
     final habits = ref.watch(habitProvider);
+    final selectedDate = ref.watch(selectedDateProvider);
 
     final theme = Theme.of(context);
+
+
+    final safeSelectedDate = selectedDate ?? DateTime.now();
+
+    // Szűrés az adott nap habitjaira
+    final filteredHabits = habits.where((habit) {
+
+      final habitDate = DateTime(
+        habit.startDate.year,
+        habit.startDate.month,
+        habit.startDate.day,
+      );
+      final selected = DateTime(safeSelectedDate.year, safeSelectedDate.month, safeSelectedDate.day);
+      return habitDate == selected;
+    }).toList();
 
     return Scaffold(
       appBar: AppBar(
@@ -32,35 +49,38 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
       ),
       body: Column(
         children: [
-          Calendar(),
+          const Calendar(),
           const SizedBox(height: 20),
-          habits.isNotEmpty
+          filteredHabits.isNotEmpty
               ? Expanded(
-                child: GridView.builder(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 10,
-                    vertical: 5,
-                  ),
-                  itemCount: habits.length,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    crossAxisSpacing: 12,
-                    mainAxisSpacing: 12,
-                    childAspectRatio: 1,
-                  ),
-                  itemBuilder: (context, index) {
-                    final habit = habits[index];
-                    return HabitCard(habit: habit);
-                  },
-                ),
-              )
-              : const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text(
-                  'No habit saved yet.',
-                  style: TextStyle(fontSize: 16, fontStyle: FontStyle.italic),
-                ),
+            child: GridView.builder(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              itemCount: filteredHabits.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 12,
+                mainAxisSpacing: 12,
+                childAspectRatio: 1,
               ),
+              itemBuilder: (context, index) {
+                final habit = filteredHabits[index];
+                return HabitCard(habit: habit);
+              },
+            ),
+          )
+              : Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'No habit for selected day.',
+              style: TextStyle(
+                fontSize: 24,
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w700,
+                color: theme.colorScheme.onSurface,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
         ],
       ),
     );
