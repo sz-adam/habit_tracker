b6ccba5610dba4d1956b502a2ae71f9d686831c6
Szabó Ádám <112242221+sz-adam@users.noreply.github.com>
2025-05-14 10:00:39 +0200

    days of habits marked, plus dialog for new habit

--- lib/widget/Calendar.dart
+++ lib/widget/Calendar.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:table_calendar/table_calendar.dart';
 
+import '../model/habit_model.dart';
 import '../riverpod/date_provider.dart';
-import '../screens/add_habit.dart'; // Importáld a providert
+import '../riverpod/habit_provider.dart';
+import '../screens/add_habit.dart';
+import '../utils/day_option_dialog.dart';
 
 class Calendar extends ConsumerStatefulWidget {
   const Calendar({super.key});
@@ -19,22 +22,43 @@ class _CalendarState extends ConsumerState<Calendar> {
   @override
   Widget build(BuildContext context) {
     final selectedDay = ref.watch(selectedDateProvider);
+    final habits = ref.watch(habitProvider);
+
+    // habitok csoportosítása dátum alapján
+    Map<DateTime, List<Habit>> events = {};
+    for (var habit in habits) {
+      final date = DateTime(
+        habit.startDate.year,
+        habit.startDate.month,
+        habit.startDate.day,
+      );
+      events.putIfAbsent(date, () => []).add(habit);
+    }
+    //eltávolítja az órát, percet, másodpercet. Visszaadjuk a habitokat
+    List<Habit> _getEventsForDay(DateTime day) {
+      final key = DateTime(day.year, day.month, day.day);
+      return events[key] ?? [];
+    }
 
     return TableCalendar(
       firstDay: DateTime.utc(2020, 1, 1),
       lastDay: DateTime.utc(2030, 12, 31),
       focusedDay: _focusedDay,
       selectedDayPredicate: (day) => isSameDay(selectedDay, day),
-      onDaySelected: (selectedDay, focusedDay) {
+      onDaySelected: (selectedDay, focusedDay) async {
         ref.read(selectedDateProvider.notifier).state = selectedDay;
         setState(() {
           _focusedDay = focusedDay;
         });
+        final result = await showDayOptionsDialog(context);
 
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => AddHabit()),
-        );
+        //navigáció
+        if (result == 'add') {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AddHabit()),
+          );
+        }
       },
       calendarFormat: _calendarFormat,
       onFormatChanged: (format) {
@@ -43,7 +67,35 @@ class _CalendarState extends ConsumerState<Calendar> {
         });
       },
       startingDayOfWeek: StartingDayOfWeek.monday,
+      eventLoader: _getEventsForDay,
+      calendarBuilders: CalendarBuilders(
+        markerBuilder: (context, date, events) {
+          if (events.isNotEmpty) {
+            return Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children:
+                  events.map((e) {
+                    final habit = e as Habit;
+                    return Container(
+                      width: 6,
+                      height: 6,
+                      margin: const EdgeInsets.symmetric(horizontal: 1.5),
+                      decoration: BoxDecoration(
+                        color: habit.color,
+                        shape: BoxShape.circle,
+                      ),
+                    );
+                  }).toList(),
+            );
+          }
+          return null;
+        },
+      ),
       calendarStyle: CalendarStyle(
+        holidayDecoration: BoxDecoration(
+          color: Colors.green,
+          shape: BoxShape.circle,
+        ),
         todayDecoration: BoxDecoration(
           color: Theme.of(context).colorScheme.primary.withOpacity(0.6),
           shape: BoxShape.circle,
@@ -54,7 +106,6 @@ class _CalendarState extends ConsumerState<Calendar> {
         ),
       ),
       headerStyle: HeaderStyle(
-        //honap style
         titleTextStyle: TextStyle(
           fontSize: 16,
           fontWeight: FontWeight.w600,
